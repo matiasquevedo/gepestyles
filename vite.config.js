579c6da5ed1d51a5b3b40b998f5bacd1718aeb4d
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2023-02-15 13:30:41 -0300

    change to /src, v1.7.0

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 // vite.config.js
+import path from "path";
+
 export default {
   // config options
+  root: './src',
   build:{
+  	outDir: '../dist',
   	assetsDir:'css',
   	rollupOptions: {
   	  output: {
   	    assetFileNames: "css/gepestyles[extname]",
   	  },
   	},
-  }
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
 
 }
